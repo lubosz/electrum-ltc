d06bd8853b631b6b8f21cbac13f821afb08b8d03
Lubosz Sarnecki <lubosz@gmail.com>
2024-10-06 10:55:14 +0200

    ecc_fast: Drop .0 suffix when loading libsecp256k1.
    This suffix changes on newer versions, but the symlink without a version
    will always be there.
    Fixes loading the library with the current .2 version.

--- electrum_ltc/ecc_fast.py
+++ electrum_ltc/ecc_fast.py
@@ -46,8 +46,8 @@ def load_library():
     elif 'ANDROID_DATA' in os.environ:
         library_paths = ('libsecp256k1.so',)
     else:  # desktop Linux and similar
-        library_paths = (os.path.join(os.path.dirname(__file__), 'libsecp256k1.so.0'),
-                         'libsecp256k1.so.0')
+        library_paths = (os.path.join(os.path.dirname(__file__), 'libsecp256k1.so'),
+                         'libsecp256k1.so')
 
     exceptions = []
     secp256k1 = None
